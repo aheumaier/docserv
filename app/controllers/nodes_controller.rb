b92f93be4e11ef3c854704633911f16848bcf0f0
Andreas Heumaier <andreas.heumaier@mondiamedia.com>
2014-08-27 11:45:12 +0200

    WIP: wiki controllers

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -13,73 +13,14 @@ class NodesController < ApplicationController
   # GET /nodes/1
   # GET /nodes/1.json
   def show
-    
     @node = Node.find(params[:id].gsub('_', '.'))
-    
-    node_facts = Node.facts( params[:id].gsub('_', '.') )
+    @node_facts = @node.facts
+    @node_artifact_id = @node.artifact_id
+    @wiki = Page.find(@node_artifact_id)
+
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @node }
-    end
-  end
-
-  # GET /nodes/new
-  # GET /nodes/new.json
-  def new
-    @node = Node.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @node }
-    end
-  end
-
-  # GET /nodes/1/edit
-  def edit
-    @node = Node.find(params[:id])
-  end
-
-  # POST /nodes
-  # POST /nodes.json
-  def create
-    @node = Node.new(params[:node])
-
-    respond_to do |format|
-      if @node.save
-        format.html { redirect_to @node, notice: 'Node was successfully created.' }
-        format.json { render json: @node, status: :created, location: @node }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @node.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /nodes/1
-  # PUT /nodes/1.json
-  def update
-    @node = Node.find(params[:id])
-
-    respond_to do |format|
-      if @node.update_attributes(params[:node])
-        format.html { redirect_to @node, notice: 'Node was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @node.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /nodes/1
-  # DELETE /nodes/1.json
-  def destroy
-    @node = Node.find(params[:id])
-    @node.destroy
-
-    respond_to do |format|
-      format.html { redirect_to nodes_url }
-      format.json { head :no_content }
     end
   end
 end
